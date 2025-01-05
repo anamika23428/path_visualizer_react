e74dafc6f361dbb87bfbf36f8802dc56f75d5591
anamika123 <anamika23428@gmail.com>
2025-01-05 17:26:43 +0530

    Update tailwind.config.js

--- path_visualizer/tailwind.config.js
+++ path_visualizer/tailwind.config.js
@@ -6,6 +6,15 @@ export default {
   ],
   theme: {
     extend: {
+      screens: {
+        sm: '640px', // Small screens (default Tailwind sm)
+        md: '768px', // Medium screens (default Tailwind md)
+        lg: '1024px', // Large screens (default Tailwind lg)
+        xl: '1280px', // Extra large screens (default Tailwind xl)
+        '2xl': '1536px', // 2x Extra large screens (default Tailwind 2xl)
+        // Add custom breakpoints if needed
+        xs: '480px', // Extra small screens
+      },
       fontFamily:{
         sans: ['Roboto' , 'sans-serif']
       }
